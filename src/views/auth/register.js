e5682b953f855464ea5b12a373bfc8cc396d1f05
med-zino <m.feddag@esi-sba.dz>
2020-03-10 01:03:49 +0100

    before final resault (regestration)

--- src/views/auth/register.js
+++ src/views/auth/register.js
@@ -1,16 +1,18 @@
+
+
 var xa;
 xa = 'lab1';
 xb = 'lab2'
 xc = 'lab3'
 xd = 'lab4'
 function zozo (x) {
-document.querySelector('.' + x).classList.toggle('active');
+document.querySelector('.' + x).classList.add('active');
 console.log(x)
 }
-document.querySelector('.lab1').addEventListener('click', zozo (xa));
-document.querySelector('.lab2').addEventListener('click', zozo (xb));
-document.querySelector('.lab3').addEventListener('click', zozo (xc));
-document.querySelector('.lab3').addEventListener('click', zozo (xd));
+document.querySelector('.lab1').addEventListener('focus', zozo (xa));
+document.querySelector('.lab2').addEventListener('focus', zozo (xb));
+document.querySelector('.lab3').addEventListener('focus', zozo (xc));
+document.querySelector('.lab4').addEventListener('focus', zozo (xd));
 document.querySelector('.form-control').addEventListener('focus', function (){
 document.querySelector('.byad').style.display = 'none';
 });
