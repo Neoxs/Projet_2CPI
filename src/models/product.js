8ad6b60d72798632c175886a414ec7e7ffeda662
Neoxs <m.abdelkaderkharoubi@esi-sba.dz>
2020-03-14 17:36:33 +0100

    adding Product model

---
+++ src/models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+//const validator = require('validator')
+//const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
