fb0da422183174cc4e51ebdc9cceb39db7175a51
med-zino <m.feddag@esi-sba.dz>
2020-03-15 18:04:12 +0100

    jj

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -5,6 +5,7 @@ xa = 'lab1';
 xb = 'lab2'
 xc = 'lab3'
 xd = 'lab4'
+xx = 'lab5'
 function zozo (x) {
 document.querySelector('.' + x).classList.add('active');
 console.log(x)
@@ -13,6 +14,7 @@ document.querySelector('.lab1').addEventListener('focus', zozo (xa));
 document.querySelector('.lab2').addEventListener('focus', zozo (xb));
 document.querySelector('.lab3').addEventListener('focus', zozo (xc));
 document.querySelector('.lab4').addEventListener('focus', zozo (xd));
+document.querySelector('.lab5').addEventListener('focus', zozo (xx));
 document.querySelector('.form-control').addEventListener('focus', function (){
 document.querySelector('.byad').style.display = 'none';
 });
