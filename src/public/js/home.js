50e5b5d3436ec08cc267ec9c8478c079654e523f
med-zino <m.feddag@esi-sba.dz>
2020-03-24 18:53:19 +0100

    first structure of home page

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,3 +1,18 @@
 document.querySelector('.form-control').addEventListener('focus', function (){
     document.querySelector('.byad').style.display = 'none';
 });
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
